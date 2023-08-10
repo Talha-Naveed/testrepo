2086812bcf2eb18004e695378bcc6042b255120d
Talha-Naveed <135945888+Talha-Naveed@users.noreply.github.com>
2023-08-10 22:43:27 +0500

    Add files via upload
    Uploading a file

---
+++ dataTypes.py
@@ -0,0 +1,26 @@
+a = complex(8, 2)  # refers to imaginary numbers
+b = True  # refers to boolean numbers
+c = "Harry"  # refers to string
+d = None  # none-type
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+
+# common way to write a list
+# lists are mutable
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+# way to write a tuple
+# tuples are immutable
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+# way to write a dictionary
+dict1 = {"name": "Sakshi", "age": 20, "canVote": True}
+print(dict1)
